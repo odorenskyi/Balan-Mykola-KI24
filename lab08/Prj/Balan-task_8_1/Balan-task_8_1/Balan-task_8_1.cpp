b24fd9985b529fd0c2e7e2ae40316b72c140e385
Boonapart <kolyha101@gmail.com>
2025-04-07 19:59:43 +0300

    Додано папку TestDriver до prj. Реалізовано програму для перевірки модуля ModulesBalan.

---
+++ lab08/Prj/Balan-task_8_1/Balan-task_8_1/Balan-task_8_1.cpp
@@ -0,0 +1,13 @@
+﻿#include <cmath>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+double calculateS(double x, double y, double z) {
+    double numerator = abs(z * z * exp(x));
+    double denominator = 12 * x + (y * y - M_PI * sqrt(z));
+
+    double result = x * x - pow(y, 3) + sqrt(numerator / denominator);
+    return result;
+}
